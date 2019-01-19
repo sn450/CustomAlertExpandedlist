d2d4a25213448f5e3c37dd32381753eea24f39e7
Tata Strive <snehal.jadhav450@gmail.com>
2019-01-19 09:44:32 +0530

    Initial commit1

---
+++ app/src/main/java/sample/tatastrive/snehal/customalert/CustomAlertActivity.java
@@ -0,0 +1,45 @@
+package sample.tatastrive.snehal.customalert;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class CustomAlertActivity extends AppCompatActivity {
+final Context context=this;
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Click", Toast.LENGTH_SHORT).show();
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.activity_main);
+                Button dialogButton = dialog.findViewById(R.id.button);
+                dialogButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v1) {
+                        dialog.dismiss();
+                        Toast.makeText(CustomAlertActivity.this,"Dismissed..!!",Toast.LENGTH_LONG);
+
+                    }
+                });
+                //
+                Intent intent = new Intent(CustomAlertActivity.this,Main3Activity.class);
+                startActivity(intent);
+                dialog.show();
+
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/sample/tatastrive/snehal/customalert/ExpandableListAdapter.java
@@ -0,0 +1,64 @@
+package sample.tatastrive.snehal.customalert;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+class ExpandableListAdapter extends BaseExpandableListAdapter {
+    public ExpandableListAdapter(Main3Activity main3Activity, List<String> listDataHeader, HashMap<String, List<String>> listDataChild) {
+
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
